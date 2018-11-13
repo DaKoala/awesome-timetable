21f06734b1a65381a895e9da2befe21b37adcf5f
billyzou <yz3352@nyu.edu>
2018-11-12 21:19:13 -0500

    feat: initialize the project and add some input in the home page

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <el-form :model="formAccount" label-width="80px">
+            <el-form-item label="Email">
+                <el-input v-model="formAccount.email"></el-input>
+            </el-form-item>
+            <el-form-item label="Password">
+                <el-input v-model="formAccount.password" type="password"></el-input>
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+    name: 'home',
+    components: {},
+    data() {
+        return {
+            formAccount: {
+                email: '',
+                password: '',
+            },
+        };
+    },
+};
+</script>
