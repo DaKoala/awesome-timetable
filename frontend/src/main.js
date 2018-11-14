6350b65bf537a80101b6134cb86f9ab2a08405d1
billyzou <yz3352@nyu.edu>
2018-11-14 14:51:26 -0500

    feat: add color and language configuration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/en';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 
 new Vue({
     router,
