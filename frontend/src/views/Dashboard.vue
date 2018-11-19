d7a9488fcd7fe51b612454c6188ef9b41e8214a5
billyzou <yz3352@nyu.edu>
2018-11-19 14:49:55 -0500

    feat: initialize nav bar in dashboard

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,6 +1,16 @@
 <template>
     <div>
-        Hello, {{user.name}}
+        <el-menu mode="horizontal">
+            <el-button type="primary">New Schedule</el-button>
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                    {{user.name}}<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>Sign out</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </el-menu>
     </div>
 </template>
 
@@ -8,7 +18,7 @@
 import validateAuth from '../util/auth';
 
 export default {
-    name: 'dashboard',
+    name: 'Dashboard',
     data() {
         return {
 
