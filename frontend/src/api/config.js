eb7ee9e1508ba2cdf2ddd3c101eae23ccdf115c2
billyzou <yz3352@nyu.edu>
2018-11-18 23:39:14 -0500

    feat: configure the api url using environment variable

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:3000';
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://linserv1.cims.nyu.edu:38499' : 'http://localhost:3000';
+
 const apiPath = {
     checkExist: '/user/checkExist',
     register: '/user/register',
