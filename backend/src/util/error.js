9b4c1a98758002346465d8a0dda8165fc60a7db2
billyzou <yz3352@nyu.edu>
2018-11-25 14:48:29 -0500

    refactor: change error handler to a class

--- backend/src/util/error.js
+++ backend/src/util/error.js
@@ -1,18 +1,25 @@
-function serverError(res) {
-    res.status(404);
-    res.send({
-        message: 'Server error',
-    });
+class ErrorHandler {
+    constructor(defaultMsg = 'Unknown error') {
+        this.defaultMsg = defaultMsg;
+    }
+
+    serverError(res) {
+        this.defaultMsg = 'Server error';
+        res.status(404);
+        res.send({
+            message: this.defaultMsg,
+        });
+    }
+
+    customError(res, msg = 'Unknown error') {
+        this.defaultMsg = msg;
+        res.status(404);
+        res.send({
+            message: this.defaultMsg,
+        });
+    }
 }
 
-function customError(res, msg = 'Unknown error') {
-    res.status(404);
-    res.send({
-        message: msg,
-    });
-}
+const error = new ErrorHandler();
 
-module.exports = {
-    serverError,
-    customError,
-};
+module.exports = error;
