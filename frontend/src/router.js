371fe031d482be4d4bf55694cbff40f344991dd9
billyzou <yz3352@nyu.edu>
2018-11-30 16:08:59 -0500

    style: delete comments

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -19,10 +19,6 @@ const router = new Router({
         {
             path: '/dashboard',
             name: 'dashboard',
-            // route level code-splitting
-            // this generates a separate chunk (about.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            // component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
             component: Dashboard,
             meta: {
                 title: 'Dashboard',
